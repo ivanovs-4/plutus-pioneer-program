3353ac3d645823ac66668463f6e852b76c75d5db
Sergey Ivanov <ivanovs-4@yandex.ru>
2025-02-04 10:59:55 +0400

    Week05 homework

--- code/Week05/homework/Homework1.hs
+++ code/Week05/homework/Homework1.hs
@@ -1,21 +1,36 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell   #-}
 
 module Homework1 where
 
+import           Plutus.V1.Ledger.Interval (contains, to)
 import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy, POSIXTime,
-                                       PubKeyHash, ScriptContext,
+                                       PubKeyHash,
+                                       ScriptContext (scriptContextTxInfo),
+                                       TxInfo (txInfoValidRange),
                                        mkMintingPolicyScript)
+import           Plutus.V2.Ledger.Contexts (txSignedBy)
 import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (False), ($))
+import           PlutusTx.Prelude     (Bool, traceIfFalse, ($), (&&))
 import           Utilities            (wrapPolicy)
 
 {-# INLINABLE mkDeadlinePolicy #-}
 -- This policy should only allow minting (or burning) of tokens if the owner of the specified PubKeyHash
 -- has signed the transaction and if the specified deadline has not passed.
 mkDeadlinePolicy :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkDeadlinePolicy _pkh _deadline () _ctx = False -- FIX ME!
+mkDeadlinePolicy pkh deadline () ctx =
+    (traceIfFalse "missing signature" $ txSignedBy (scriptContextTxInfo ctx) pkh)
+    &&
+    (traceIfFalse "deadline passed" $ checkDeadline)
+
+    where
+        txInfo :: TxInfo
+        txInfo = scriptContextTxInfo ctx
+
+        checkDeadline :: Bool
+        checkDeadline = contains (to deadline) (txInfoValidRange txInfo)
 
 {-# INLINABLE mkWrappedDeadlinePolicy #-}
 mkWrappedDeadlinePolicy :: PubKeyHash -> POSIXTime -> BuiltinData -> BuiltinData -> ()
--- code/Week05/homework/Homework2.hs
+++ code/Week05/homework/Homework2.hs
@@ -5,22 +5,58 @@
 
 module Homework2 where
 
+import           Plutus.V1.Ledger.Value     (flattenValue, adaToken)
 import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy,
-                                       ScriptContext, TokenName, TxOutRef,
+                                       ScriptContext (scriptContextTxInfo),
+                                       TxInfo (txInfoInputs, txInfoMint),
+                                       TxOutRef,
+                                       TxInInfo (txInInfoOutRef),
                                        mkMintingPolicyScript)
 import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (False), ($), (.))
+import           PlutusTx.Prelude     (Bool (False), traceIfFalse, any,
+                                             Eq ((==)), ($), (&&))
 import           Utilities            (wrapPolicy)
+
+
 
 {-# INLINABLE mkEmptyNFTPolicy #-}
 -- Minting policy for an NFT, where the minting transaction must consume the given UTxO as input
 -- and where the TokenName will be the empty ByteString.
 mkEmptyNFTPolicy :: TxOutRef -> () -> ScriptContext -> Bool
-mkEmptyNFTPolicy _oref () _ctx = False -- FIX ME!
+mkEmptyNFTPolicy oref () ctx =
+       traceIfFalse "UTxO not consumed"   hasUTxO
+    && traceIfFalse "wrong amount minted" checkMintedNameAndAmount
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo ctx
+
+    hasUTxO :: Bool
+    hasUTxO = any (\i -> txInInfoOutRef i == oref) $ txInfoInputs info
+
+    checkMintedNameAndAmount :: Bool
+    checkMintedNameAndAmount = case flattenValue (txInfoMint info) of
+        [(_, name, amount)] -> name == adaToken && amount == 1
+        _                   -> False
+
+-- {-# INLINABLE mkWrappedEmptyNFTPolicy #-}
+-- mkWrappedEmptyNFTPolicy :: TxOutRef -> BuiltinData -> BuiltinData -> ()
+-- mkWrappedEmptyNFTPolicy = wrapPolicy . mkEmptyNFTPolicy
+
+-- nftPolicy :: TxOutRef -> MintingPolicy
+-- nftPolicy oref = mkMintingPolicyScript $
+--    $$(PlutusTx.compile [|| mkWrappedEmptyNFTPolicy ||])
+--       `PlutusTx.applyCode` PlutusTx.liftCode oref
 
 {-# INLINABLE mkWrappedEmptyNFTPolicy #-}
-mkWrappedEmptyNFTPolicy :: TxOutRef -> BuiltinData -> BuiltinData -> ()
-mkWrappedEmptyNFTPolicy = wrapPolicy . mkEmptyNFTPolicy
+mkWrappedEmptyNFTPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkWrappedEmptyNFTPolicy oref = wrapPolicy $
+  mkEmptyNFTPolicy (PlutusTx.unsafeFromBuiltinData oref)
 
-nftPolicy :: TxOutRef -> TokenName -> MintingPolicy
-nftPolicy oref tn = mkMintingPolicyScript $ $$(PlutusTx.compile [|| mkWrappedEmptyNFTPolicy ||]) `PlutusTx.applyCode` PlutusTx.liftCode oref
+nftCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> ())
+nftCode = $$(PlutusTx.compile [|| mkWrappedEmptyNFTPolicy ||])
+
+nftPolicy :: TxOutRef -> MintingPolicy
+nftPolicy oref = mkMintingPolicyScript $
+   nftCode
+      `PlutusTx.applyCode` PlutusTx.liftCode (PlutusTx.toBuiltinData oref)
+
